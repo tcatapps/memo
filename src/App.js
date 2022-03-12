f30b89f011617770a9c1956344503609ecc693a0
tarik <tarikulemma913@gmail.com>
2022-03-12 18:40:08 +0300

    - created LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-       Hello Merry!
+       <LandingPage title={'This is a title'} paragraph = {'lorem ipsum de solo....'} />
     </div>
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/lading_page.css';
+
+const LandingPage = ({title, paragraph}) => {
+    return (  
+        <div className='flex-xample'>
+            <div className='left'>left</div>
+            <div className='right'>right</div>
+        </div>
+    );
+}
+ 
+export default LandingPage;
